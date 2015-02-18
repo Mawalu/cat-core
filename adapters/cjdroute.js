e0cd9106c793610c31894c0a02614728b70dc92f
mawalu <web@mawalabs.de>
2015-02-18 15:19:28 +0000

    Added peer managing code to cjdroute adapter

--- adapters/cjdroute.js
+++ adapters/cjdroute.js
@@ -8,9 +8,33 @@ cjdroute.connect = function (node) {
     });
 };
 
+cjdroute.setup = function () {
+    // load peers
+    // init hartbeat checks
+};
+
 cjdroute.ping = function () {
     var channel = cjdroute.connection.ping();
     cjdroute.connection.on(channel, console.log);
+};
+
+cjdroute.addPeer = function (peer) {
+    if( peer.ip ) { // outbound
+        var channel = cjdroute.connection.udpInterface.beginConnection(peer);
+        cjdroute.connection.once(channel, console.log);
+    } else { // inbound
+        var channel = cjdroute.connection.authorizedPasswords.add(peer);
+        cjdroute.connection.once(channel, console.log);
+    }
+};
+
+cjdroute.removePeer = function (peer) {
+    if( peer.ip ) { // outbound
+        // as far as i know, we can't disconnect from peers
+    } else { // inbound
+        var channel = cjdroute.connection.authorizedPasswords.remove(peer);
+        cjdroute.connection.once(channel, console.log);
+    }
 };
 
 module.exports = cjdroute;
