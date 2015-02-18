0f072ec3b0c0c28eee11c9a5e644d04c9426e85b
mawalu <web@mawalabs.de>
2015-02-18 15:59:55 +0000

    Cleand up nodes plugin + added comments

--- plugins/nodes.js
+++ plugins/nodes.js
@@ -7,13 +7,29 @@ nodes.init = function(db, cjdns, router) {
     nodes.cjdns = cjdns; 
 
     router.route('/nodes')
-        .get(function(req, res) {
+        .get(function(req, res) { // get all nodes
             res.json({status: "succes", data: nodes.db('nodes').value()})
          })
-        .post(nodes.newNode);
+        .post(function(req, res) { // add new node
+            if(!req.body.cjdnsIp || !req.body.adminPassword || !req.body.adminPort || !req.body.adapter) {
+                return res.json({status: "error", data: {"msg": "parameters missing"}});
+            }
+
+            var id = uuid.v4();
+
+            nodes.db('nodes').push({ id: id,
+                                     cjdnsIp: req.body.cjdnsIp,
+                                     adminPassword: req.body.adminPassword,
+                                     adminPort: req.body.adminPort,
+                                     adapter: req.body.adapter,
+                                     comment: req.body.comment,
+                                     name: req.body.name }).id;
+
+            res.json({status: "succes", data: {id: id}});
+        });
 
     router.route('/nodes/:id')
-        .get(function(req, res) {
+        .get(function(req, res) { // show one node
             var result = db('nodes').find({id: req.params.id}).value();
 
             if(typeof result == "undefined") {
@@ -22,37 +38,19 @@ nodes.init = function(db, cjdns, router) {
                 res.json({status: "succes", data: result});
             }
         })
-        .put(function(req, res) {
+        .put(function(req, res) { // update node
             if(req.params.id) {
                 var node = db('nodes').find({id: req.params.id}).assign(req.body).value();
                 res.json({status: "succes", data: node}); 
             }
         })
-        .delete(function(req, res) {
+        .delete(function(req, res) { // delete node
             db('nodes').remove({id: req.params.id});
 
             res.json({status: "succes", data: ""});
         });
 
     console.log('Loaded node management plugin');
-}
-
-nodes.newNode = function(req, res) {
-    if(!req.body.cjdnsIp || !req.body.adminPassword || !req.body.adminPort || !req.body.adapter) {
-        return res.json({status: "error", data: {"msg": "parameters missing"}});
-    }
-
-    var id = uuid.v4();
-
-    nodes.db('nodes').push({ id: id,
-                             cjdnsIp: req.body.cjdnsIp,
-                             adminPassword: req.body.adminPassword,
-                             adminPort: req.body.adminPort,
-                             adapter: req.body.adapter,
-                             comment: req.body.comment,
-                             name: req.body.name }).id;
-
-    return res.json({status: "succes", data: {id: id}});
 }
 
 module.exports = nodes;
